4055d60a14f28155dae263286382266432c12ce2
TengLi <18547241+TenTen-Teng@users.noreply.github.com>
2025-03-02 18:15:30 -0800

    update module import

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
-from data_process import (
+from .data_process import (
     crops_line_dataset, gini_line_dataset, wage_dataset, map_dataset,
     number_card_dataset
     )
-from helper import calculate_change
+from .helper import calculate_change
