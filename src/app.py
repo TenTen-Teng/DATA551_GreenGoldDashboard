ca335eadc6907eb3ef0af060a3c08dd3fec769cc
TengLi <18547241+TenTen-Teng@users.noreply.github.com>
2025-03-10 11:34:36 -0700

    update run_server

--- src/app.py
+++ src/app.py
@@ -12,11 +12,11 @@ import plotly.graph_objects as go
 import json
 import pandas as pd
 
-from .data_process import (
+from data_process import (
     crops_line_dataset, gini_line_dataset, wage_dataset, map_dataset,
     number_card_dataset
     )
-from .helper import calculate_change
+from helper import calculate_change
 
 # absolute path to the root of the repository
 repo_root = Path(__file__).resolve().parents[1]
@@ -966,7 +966,5 @@ server = app.server
 if __name__ == '__main__':
     app.run_server(
         debug=False,
-        # host='142.231.93.127', 
-        host = '127.0.0.1',
-        port=8050
+        host='0.0.0.0', port=int(os.environ.get('PORT', 8050)),
     )
